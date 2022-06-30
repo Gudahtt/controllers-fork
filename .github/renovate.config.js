6d9a2fbe9f52508958a5c8f0c7cf806bf165d089
Mark Stacey <markjstacey@gmail.com>
2022-06-30 12:35:52 -0230

    test

--- .github/renovate.config.js
+++ .github/renovate.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   // Self-Hosted configuration
   allowScripts: false,
+  customEnvVariables: {
+    SKIP_ALLOW_SCRIPTS: 'true',
+  },
   platform: 'github',
   onboarding: false,
 }
